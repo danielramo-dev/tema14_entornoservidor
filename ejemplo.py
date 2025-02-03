457a58d2b228013dd4c0ab4d04f941d0f6b62210
danidaw15 <drm0027@alu.medac.es>
2025-02-03 20:57:46 +0100

    ejercicios del tema 14

--- ejemplo.py
+++ ejemplo.py
@@ -4,6 +4,20 @@ from bs4 import BeautifulSoup
 url = "https://es.wikipedia.org/wiki/Wikipedia:Portada"
 response = requests.get(url)
 
-soup = BeautifulSoup(response.text,"html.parser")
+soup = BeautifulSoup(response.text, "html.parser")
+
 titulo = soup.title.text
-print(f"El titulo es {titulo}")
+print(f"El título de la página es: {titulo}")
+
+actualidad_section = soup.find("div", id="main-cur")
+
+if actualidad_section:
+    noticias = actualidad_section.find_all("li") 
+
+    print("\n Noticias de Actualidad en Wikipedia:")
+    for index, noticia in enumerate(noticias, 1):
+        texto_noticia = noticia.get_text(" ", strip=True)  
+        print(f"{index}. {texto_noticia}")
+
+else:
+    print("No se encontró la sección de actualidad.")
