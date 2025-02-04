fba3a59eafa71b49f3d5768162db1b48be8fbdb7
danielramo-dev <danielramo2305@gmail.com>
2025-02-04 13:38:50 +0100

    ejercicios del tema 14

--- meteorologia.php
+++ meteorologia.php
@@ -16,7 +16,6 @@
     curl_close($ch);
 
     $data = json_decode($response, true);
-
     $fecha_hoy = date('Y-m-d');
     $fecha_api = $data['fecha'];
 
@@ -28,9 +27,32 @@
         echo "<h1>El Tiempo en $nombre_ciudad</h1>";
         echo "<p><strong>Fecha:</strong> $fecha_api</p>";
         echo "<p><strong>Temperatura Actual:</strong> $temperatura</p>";
-        echo "<p><strong>Estado del Cielo:</strong> $estado_cielo</p>";
-    } else {
-        echo "<p>No hay información meteorológica actualizada para hoy ($fecha_hoy).</p>";
+        echo "<p><strong>Estado del Cielo:</strong> $estado_cielo</p><br>";}
+
+    $municipio = $data['municipio']['NOMBRE'];
+    $proximos_dias = $data['proximos_dias'];
+   
+
+
+    foreach ($proximos_dias as $dia) {
+        $fecha = $dia['@attributes']['fecha'];
+
+        if ($fecha !== $fecha_hoy) {
+            
+            $temperatura_maxima = $dia['temperatura']['maxima'] . '°C';
+            $temperatura_minima = $dia['temperatura']['minima'] . '°C';
+            echo "<p><strong>Fecha:</strong> $fecha</p>";
+            echo "<p><strong>Temperatura Máxima:</strong> $temperatura_maxima</p>";
+            echo "<p><strong>Temperatura Mínima:</strong> $temperatura_minima</p>";
+        }
+
+        if (is_array($dia['estado_cielo_descripcion'])) {
+            $estado_cielo = $dia['estado_cielo_descripcion'][0]; 
+        } else {
+            $estado_cielo = $dia['estado_cielo_descripcion']; 
+        }
+
+        echo "<p><strong>Estado del Cielo:</strong> $estado_cielo</p><br>";
     }
     ?>
 </body>
